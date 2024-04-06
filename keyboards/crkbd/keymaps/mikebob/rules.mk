5b222a747c9f69d1cc2df5f43310fd13ae5b9c7a
Mike Bobiney <mikebob@users.noreply.github.com>
2024-04-06 11:07:49 -0400

    combining parts of Vial and HellSingCoder

---
+++ keyboards/crkbd/keymaps/mikebob/rules.mk
@@ -0,0 +1,21 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+
+MOUSEKEY_ENABLE = no     # Mouse keys
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes    # Enable WS2812 RGB underlight.
+VIA_ENABLE      = yes    # Enable VIA
+OLED_ENABLE     = yes
+LTO_ENABLE      = yes
+WPM_ENABLE = yes
+
+EXTRAKEY_ENABLE     = yes
+COMBO_ENABLE        = no
+
+QMK_SETTINGS        = no
+
+# RGBLIGHT_ENABLE     = yes
+# RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+# OLED_ENABLE         = no
+# OLED_DRIVER         = SSD1306
